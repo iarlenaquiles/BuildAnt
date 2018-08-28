4622b6522cbab4070711b1b320fc169a41299613
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-08-28 15:56:42 -0300

    Usando o showmessagedialog

--- src/com/br/helloworld/Main.java
+++ src/com/br/helloworld/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		JOptionPane.showConfirmDialog(null, "Hello World");
+		JOptionPane.showMessageDialog(null, "Hello World");
 	}
 
 }
